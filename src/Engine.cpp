2333e7dd27037da73817f85d4c541a87526a3e83
lvke <lvke>
2023-10-03 21:32:46 +0800

    test cmake

---
+++ src/Engine.cpp
@@ -0,0 +1,7 @@
+#include "Engine.h"
+
+void Engine::init()
+{
+	int i = 0;
+	i++;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
+#include "Engine.h"
 
 int main()
 {
+	Engine engine;
+	engine.init();
+
 	std::cout << "Hello, CMake" << std::endl;
+
 	return 0;
 }

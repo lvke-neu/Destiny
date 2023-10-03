0822d8faf47d948c04913cd666ddd0745be28e44
lvke <lvke>
2023-10-03 22:13:28 +0800

    cmake directx11 template

--- src/Engine/Engine.cpp
+++
@@ -1,7 +0,0 @@
-#include "Engine.h"
-
-void Engine::init()
-{
-	int i = 0;
-	i++;
-}
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,27 @@
-#include <iostream>
-#include "Engine/Engine.h"
+#include "GameApp.h"
 
-int main()
+
+
+int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE prevInstance,
+    _In_ LPSTR cmdLine, _In_ int showCmd)
 {
-	Engine engine;
-	engine.init();
+    // 这些参数不使用
+    UNREFERENCED_PARAMETER(prevInstance);
+    UNREFERENCED_PARAMETER(cmdLine);
+    UNREFERENCED_PARAMETER(showCmd);
+    // 允许在Debug版本进行运行时内存分配和泄漏检测
+#if defined(DEBUG) | defined(_DEBUG)
+    _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+#endif
 
-	std::cout << "Hello, CMake" << std::endl;
+    GameApp theApp(hInstance, L"DirectX11 Initialization", 1280, 720);
 
-	return 0;
+    if (!theApp.Init())
+        return 0;
+
+    return theApp.Run();
 }
+
+
+
+

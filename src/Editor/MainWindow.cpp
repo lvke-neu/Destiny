dc17bb424dcded1a8c6b9bd73e5af12a6c7445dd
ke.lv <lvke>
2023-11-20 17:26:06 +0800

    qt layout

--- src/Editor/MainWindow.cpp
+++ src/Editor/MainWindow.cpp
@@ -9,6 +9,12 @@ MainWindow::MainWindow(QWidget *parent) :
     ui->setupUi(this);  
     m_renderWindow = new RenderWindow(this);
     setCentralWidget(m_renderWindow);
+
+    ui->treeWidget->setHeaderLabel({ "Node" });
+    QTreeWidgetItem* node = new QTreeWidgetItem(ui->treeWidget);
+    node->setText(0, "aa");
+    QTreeWidgetItem* node2 = new QTreeWidgetItem(node);
+    node2->setText(0, "bb");
 }
 
 MainWindow::~MainWindow()

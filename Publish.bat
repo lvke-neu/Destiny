8542a3e6225361c17837313f9d368dbf410de67e
lvke <lvke>
2025-01-12 12:54:34 +0800

    set assimp in cmake

--- Publish.bat
+++
@@ -1,5 +0,0 @@
-xcopy /E /I /y .\builtin\*.* .\build\Debug\builtin
-xcopy /E /I /y .\builtin\*.* .\build\Release\builtin 
-
-xcopy /E /I /y .\3rdparty\assimp\assimp-vc140-mtd.dll .\build\Debug
-xcopy /E /I /y .\3rdparty\assimp\assimp-vc140-mt.dll .\build\Release 
